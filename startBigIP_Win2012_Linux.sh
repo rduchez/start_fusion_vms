48df833158dd93e60fcb84c70cdf260720a024c3
romain <rduchez@yahoo.com>
2017-04-21 09:43:39 -0700

    added: Start -> load default config on BIG-IP Stop -> create an scf and a ucs on the BIG-IP and retrieve the scf to the local directory

--- startBigIP_Win2012_Linux.sh
+++ startBigIP_Win2012_Linux.sh
@@ -1,9 +1,35 @@
 if [ -z "$1" ]
   then
-    echo "Syntax is startBigIP_Win2012_Linux.sh [start|stop]"
-    echo "No argument supplied"
+    echo "error: No argument supplied"
+    echo "Syntax is startBigIP_Win2012_Linux.sh [start|stop] {ad}"
+    echo "AD can be started by adding ad after the start/stop mantra"
     exit 1
 fi
-/Applications/VMware\ Fusion.app/Contents/Library/vmrun $1 /Users/rduchez/Documents/virtual_machines/WindowsServer2012.vmwarevm 
+
+if [ "$1" == "stop" ]
+	then
+        echo "Stopping"
+	ssh root@192.168.155.242 'now=$(date +"%m_%d_%Y"); tmsh save sys config file config_$now.scf no-passphrase'
+	ssh root@192.168.155.242 'now=$(date +"%m_%d_%Y"); tmsh save sys ucs config_$now.ucs'
+	now=$(date +"%m_%d_%Y")
+	scp root@192.168.155.242:/var/local/scf/config_$now.scf .
+fi
+
 /Applications/VMware\ Fusion.app/Contents/Library/vmrun $1 /Users/rduchez/Documents/Virtual\ Machines.localized/BIG-IP-vLAB-02.vmwarevm
 /Applications/VMware\ Fusion.app/Contents/Library/vmrun $1 /Users/rduchez/Documents/Virtual\ Machines.localized/Linux-host.vmwarevm
+
+if [ "$2" == "ad" ]
+  then
+        /Applications/VMware\ Fusion.app/Contents/Library/vmrun $1 /Users/rduchez/Documents/virtual_machines/WindowsServer2012.vmwarevm 
+fi
+
+# load default config at start 
+if [ "$1" == "start" ]
+	then
+		echo 'Starting'
+		until $(curl -k --output /dev/null --silent --fail https://admin:admin@192.168.155.242/mgmt/toc/); do
+			printf '.'
+			sleep 20
+		done
+		ssh root@192.168.155.242 'tmsh load sys ucs bigip02_base.ucs'
+fi
